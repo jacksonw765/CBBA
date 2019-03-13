56022f77407330113ba900630fce7529baeca929
jacksksonLaptop <welchjk@mail.uc.edu>
2019-03-12 23:09:41 -0500

    added player game history

--- GameLog.py
+++ GameLog.py
@@ -1,7 +1,8 @@
 import bs4
-import pprint
 import requests
 import sql_log
+import sql_player
+
 
 class GameLog:
 
@@ -33,46 +34,49 @@ class GameLog:
             game.append(pts)
         return game
 
-    def logPlayerGames(self, soup):
+    def log_games(self, base_log, base_index):
+        try:
+            game_one = self.get_previous_game(base_log, base_index)
+            game_two = self.get_previous_game(base_log, base_index + 14)
+            game_three = self.get_previous_game(base_log, base_index + 28)
+            sql_log.create_sql_game_log(self.id, 1, game_one[0], game_one[1], game_one[2], game_one[3], game_one[4],
+                                        game_one[5], game_one[6], game_one[7], game_one[8])
+            sql_log.create_sql_game_log(self.id, 2, game_two[0], game_two[1], game_two[2], game_two[3], game_two[4],
+                                        game_two[5], game_two[6], game_two[7], game_two[8])
+            sql_log.create_sql_game_log(self.id, 3, game_three[0], game_three[1], game_three[2], game_three[3],
+                                        game_three[4],
+                                        game_three[5], game_three[6], game_three[7], game_three[8])
+        except Exception as e:
+            print("Error bitch: " + e.__str__())
 
+    # this is a horible way to do it but it works and I'm pressed for time
+    def log_player_games(self, soup):
         year = soup.find('ul', 'general-info').find_all('li')[1].getText()
         base_index = 28
         base_log = soup.find_all("td", attrs={'style': 'text-align: right;'})
-        game_list = []
         if year == "Freshman":
-            game_one = self.get_previous_game(base_log, base_index)
-            sql_log.create_sql_game_log(self.id, 1, game_one[0], game_one[1], game_one[2], game_one[3], game_one[4],
-                                        game_one[5], game_one[6], game_one[7], game_one[8])
+            self.log_games(base_log, base_index)
         if year == "Sophomore":
             base_index = base_index * 2
-            game_one = self.get_previous_game(base_log, base_index)
-            sql_log.create_sql_game_log(self.id, 1, game_one[0], game_one[1], game_one[2], game_one[3], game_one[4],
-                                        game_one[5], game_one[6], game_one[7], game_one[8])
-            #pprint.pprint(self.get_previous_game(base_log, base_index + 14))
+            self.log_games(base_log, base_index)
         if year == 'Junior':
             base_index = base_index * 3
-            game_one = self.get_previous_game(base_log, base_index)
-            sql_log.create_sql_game_log(self.id, 1, game_one[0], game_one[1], game_one[2], game_one[3], game_one[4],
-                                        game_one[5], game_one[6], game_one[7], game_one[8])
-            #pprint.pprint(self.get_previous_game(base_log, base_index + 14))
+            self.log_games(base_log, base_index)
         if year == 'Senior':
             base_index = base_index * 4
-            game_one = self.get_previous_game(base_log, base_index)
-            sql_log.create_sql_game_log(self.id, 1, game_one[0], game_one[1], game_one[2], game_one[3], game_one[4],
-                                        game_one[5], game_one[6], game_one[7], game_one[8])
-            #pprint.pprint(self.get_previous_game(base_log, base_index + 14))
+            self.log_games(base_log, base_index)
 
 
-base_id = 3934620
 root_page = 'http://www.espn.com/mens-college-basketball/player/_/id/'
 remainder = '/justin-jenifer?src=mobile'
 
+id_list = sql_player.get_ids()
 
-for id in range(0, 1):
 
-    player_id = str(base_id + id)
-    request = root_page + player_id + remainder
+for id in id_list:
+    request = root_page + str(id) + remainder
     page = requests.get(request)
     soup = bs4.BeautifulSoup(page.content, 'html.parser')
     name = soup.find_all('h1')[0].getText()
-    GameLog(player_id).logPlayerGames(soup)
+    GameLog(id).log_player_games(soup)
+    print("Games logged for: " + name)
--- sql_player.py
+++ sql_player.py
@@ -11,8 +11,6 @@ def create_sql_player(id, name, team, ppp, rpg, apg):
     except sqlite3.OperationalError as oe:
         print("Error running query: " + oe.__str__())
 
-def add_quotes(value):
-    return ' " ' + value
 
 def get_team_total_ppg(team_name):
     try:
@@ -25,6 +23,7 @@ def get_team_total_ppg(team_name):
     except sqlite3.OperationalError as oe:
         print("Error running query PPG: " + oe.__str__())
 
+
 def get_team_total_rpg(team_name):
     try:
         con = sqlite3.connect('player_data.db')
@@ -35,6 +34,7 @@ def get_team_total_rpg(team_name):
         return round(value, 2)
     except sqlite3.OperationalError as oe:
         print("Error running query RPG: " + oe.__str__())
+
 
 def get_team_total_apg(team_name):
     try:
@@ -47,4 +47,14 @@ def get_team_total_apg(team_name):
     except sqlite3.OperationalError as oe:
         print("Error running query APG: " + oe.__str__())
 
-print(get_team_total_apg("Temple Owls"))
+def get_ids():
+    try:
+        con = sqlite3.connect('player_data.db')
+        con.row_factory = lambda cursor, row: row[0]
+        query = 'SELECT ID FROM Player'
+        result = con.execute(query)
+        value = result.fetchall()
+        con.close()
+        return list(value)
+    except sqlite3.OperationalError as oe:
+        print("Error running query APG: " + oe.__str__())
